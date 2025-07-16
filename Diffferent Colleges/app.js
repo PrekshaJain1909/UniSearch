a39b6ad71a6712d7efa5b24545dbff0567d0c02f
Preksha Jain <Preksha190994@gmail.com>
2025-07-16 22:10:50 +0530

    Done

--- Diffferent Colleges/app.js
+++ Diffferent Colleges/app.js
@@ -2,7 +2,7 @@ const button = document.querySelector("#generateBtn");
 const orderedlist = document.querySelector("#orderedList");
 const input = document.querySelector("#inputt");
 const errorPara = document.querySelector("#error");
-const url = "http://universities.hipolabs.com/search?country=";
+const url = "http://universities.hipolabs.com/search?country="; // ✅ HTTPS
 
 // Fetch university data
 async function getUniversities() {
