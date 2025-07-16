744f837784ecb905d7798c93481c310686515a1c
Shaprek <prekjainsha190994@gmail.com>
2025-07-16 13:35:30 +0530

    Changed URL

--- Diffferent Colleges/app.js
+++ Diffferent Colleges/app.js
@@ -2,7 +2,7 @@ const button = document.querySelector("#generateBtn");
 const orderedlist = document.querySelector("#orderedList");
 const input = document.querySelector("#inputt");
 const errorPara = document.querySelector("#error");
-const url = "http://universities.hipolabs.com/search?country=";
+const url = "https://universities.hipolabs.com/search?country=";
 
 // Fetch university data
 async function getUniversities() {
